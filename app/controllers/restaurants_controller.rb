28567d30c247f1a38b098f38bb937fce74ad8bf1
Catalina Rojas <contacto@ojocorto.com>
2018-05-16 16:21:22 +0200

    ejercicio 1 completo

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -11,9 +11,12 @@ before_action :set_restaurant, only: [:edit, :show, :update, :destroy]
   end
 
   def create
-    @restaurant= Restaurant.new(restaurant_params)
-    @restaurant.save
-    redirect_to restaurants_path
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
   end
 
   def show
